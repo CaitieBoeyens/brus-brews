605a43028706c2d93fc3f9eae34ed22044bdf46c
caitie <caitie@localhost.localdomain>
2018-05-21 20:21:26 +0200

    history section implemented

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 #Bru's Brew
-##Read me
+##Caitlin
+##160032
 
-### About
-
-This project's structure is from [create-react-app](https://github.com/facebook/create-react-app).
-
-### Setup
+###Setup & Launch
 
 run `npm install`
+
+run `npm start` to boot the dev server.
+This allows for hot reloading so the server does not need to be rebooted when changes are made.
+
+run `npm build` to build a production ready version.
+
+###About
+
+This project's structure is from [create-react-app](https://github.com/facebook/create-react-app).
--- src/pages/home-layout/History.js
+++ src/pages/home-layout/History.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "../../css/Home.css";
+import { Line } from "react-chartjs-2";
 
 class HomeHistory extends Component {
 	constructor() {
@@ -48,30 +49,113 @@ class HomeHistory extends Component {
 			<section className="history-con">
 				<div className="container history">
 					<div className="columns is-centered">
-						<div className="heading box column is-12 has-text-centered">
-							<h2>History of Bru's Brew</h2>
-							<p>Here's a bit of background on Bru's Brew</p>
+						<div className="column is-12 has-text-centered">
+							<div className="heading block">
+								<h2>History of Bru's Brew</h2>
+								<p>Here's a bit of background on Bru's Brew</p>
+							</div>
 						</div>
 					</div>
 					<div className="columns is-centered">
-						<div className="column box is-four-fifths has-text-centered">
-							<p>graph</p>
+						<div className="column is-four-fifths has-text-centered">
+							<div className="block">
+								<Line
+									data={{
+										labels: this.state.beers.map(
+											b => b.year
+										),
+										datasets: [
+											{
+												data: this.state.beers.map(
+													b => b.numBeers
+												),
+												backgroundColor: "#f0a917ff",
+												borderColor: "black",
+												borderWidth: 5,
+												pointBorderWidth: 2
+											}
+										]
+									}}
+									options={{
+										title: {
+											display: true,
+											text:
+												"Number of beers made per year",
+											fontSize: "18",
+											fontFamily:
+												"'Open Sans', 'sans-serif'",
+											fontColor: "black",
+											padding: 40
+										},
+										layout: {
+											padding: {
+												left: 50,
+												right: 25,
+												bottom: 25
+											}
+										},
+										legend: {
+											display: false
+										},
+										scales: {
+											xAxes: [
+												{
+													scaleLabel: {
+														display: true,
+														labelString:
+															"years of operation"
+													}
+												}
+											],
+
+											yAxes: [
+												{
+													scaleLabel: {
+														display: true,
+														labelString:
+															"number of beers"
+													},
+													ticks: {
+														beginAtZero: true,
+														max: 30
+													}
+												}
+											]
+										}
+									}}
+								/>
+							</div>
 						</div>
+
 						<div className="column is-one-fifth has-text-centered">
-							<div className="box">
-								<h4>{yearOfBeer.numBeers}</h4>
+							<div className="block">
+								<h2>
+									{this.state.beers
+										.map(b => b.numBeers)
+										.reduce(function(acc, val) {
+											return acc + val;
+										})}
+								</h2>
+								<h6>total number of beers ever made</h6>
+							</div>
+							<div className="block">
+								<h2>{yearOfBeer.numBeers}</h2>
 								<h6>number of beers made in 2010</h6>
 							</div>
-							<div className="box" />
-							<div className="box" />
+							<div className="block" />
 						</div>
 					</div>
 				</div>
 			</section>
 		);
 	}
-	async componentDidMount() {
-		this._updateNumBeers(2010, await this._getMatchingBeers(2010));
+	componentWillMount() {
+		this.state.beers.forEach(async beer => {
+			this._updateNumBeers(
+				beer.year,
+				await this._getMatchingBeers(beer.year)
+			);
+		});
 	}
 
 	_updateNumBeers(year, matchingBeers) {
@@ -82,7 +166,7 @@ class HomeHistory extends Component {
 				beer.numBeers = matchingBeers.length;
 			}
 		});
-		debugger;
+
 		this.setState({ beers: beers });
 	}
 	async _getMatchingBeers(year) {
